63cfd401c0730e899673de780e81fcf325f8ae08
WIll Morris <88165849+Willmo103@users.noreply.github.com>
2023-03-30 08:07:49 -0500

    more working templates

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
 from flask import Flask, Request, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
 from wtforms import StringField, PasswordField, SubmitField, Form
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_wtf import FlaskForm
+from praw import Reddit
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.template_folder = 'templates'
@@ -13,14 +15,41 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'secret_key'
 db = SQLAlchemy(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
 
 # these will get moved to the models.py file whe I refactor the code
 class User(UserMixin, db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True)
     password = db.Column(db.String(80))
 
-class RegisterForm(Form):
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+class API_Key(db.Model):
+    __tablename__ = 'api_key'
+    id = db.Column(db.Integer, primary_key=True)
+    user_agent = db.Column(db.String(15), unique=True)
+    client_secret = db.Column(db.String(15), unique=True)
+    client_id = db.Column(db.String(15), unique=True)
+
+    def __init__(self, user_agent, client_secret, client_id):
+        self.user_agent = user_agent
+        self.client_secret = client_secret
+        self.client_id = client_id
+
+    def reddit_api(self):
+        reddit = Reddit(client_id=self.client_id,
+                     client_secret=self.client_secret,
+                     user_agent=self.user_agent)
+        return reddit
+
+class RegisterForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
     submit = SubmitField('Sign Up')
@@ -40,25 +69,32 @@ class LoginForm(FlaskForm):
         if not existing_user_username:
             raise ValidationError('That username does not exist. Please try again.')
 
+class API_Form(FlaskForm):
+    user_agent = StringField('api_key', validators=[InputRequired(), Length(min=4, max=15)])
+    client_secret = StringField('client_secret', validators=[InputRequired(), Length(min=4, max=15)])
+    client_id = StringField('client_id', validators=[InputRequired(), Length(min=4, max=15)])
+
+    def validate_api_key(self, api_key):
+        existing_api_key = API_Key.query().first()
+        if existing_api_key:
+            raise ValidationError('That API key already exists. Please choose a different one.')
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 @app.route('/')
 def index():
-    # form = LoginForm()
-    # check to see if the reddit api_key, client_id, and client_secret are set
-    # render a form to get the api_key, client_id, and client_secret before continuing
+    verify_api_key = API_Key.query.first()
+    if verify_api_key:
+        return redirect(url_for('login'))
+    elif not verify_api_key:
+        return redirect(url_for('api_key'))
+    return redirect(url_for('home'))
 
-    # if not all(['api_key', 'client_id', 'client_secret']) in os.environ:
-    #     return render_template('form.html')
-
-    # this is the main page that will render the index.html
-    # get the user auth token the request header and try to verify it
-    # if the token is valid, then the user will be redirected to the main page
-    # if the token is invalid, then the user will be redirected to the login page
-    # once the environment variables are set, the user needs to be redirected to login page
-
-    # if not 'access_token' in os.environ:
-    #     return redirect(url_for('login'))
-
-    # if the user is logged in, then the user will be redirected to the main page
+@app.route('/home')
+def home():
     return render_template('index.html')
 
 
@@ -67,8 +103,20 @@ def login():
     form = LoginForm()
     # this is the page that will render the login form 'login.html'
     # there will be a option for a new user to create an account
-
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if Bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('dashboard'))
+        return render_template('login.html', form=form, invalid=True)
     return render_template('login.html', form=form)
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 # this will be the method that will handle the login form
@@ -77,10 +125,20 @@ def register():
     form = RegisterForm()
     # this is the page that will render the register form 'register.html'
     # there will be a option for a new user to create an account
+    if form.validate_on_submit():
+        hashed_password = Bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
 
     return render_template('register.html', form=form)
 
+@app.route('/dashboard' , methods=['GET', 'POST'])
+@login_required
+def dashboard():
 
+    return render_template('dashboard.html')
 # need to setup db for flask
 # need to reasearch how to use flask with a database
 
