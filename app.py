2921c40a4ea9251f68e18337fd5f6bceb26c7de5
WIll Morris <88165849+Willmo103@users.noreply.github.com>
2023-03-30 05:31:33 -0500

    working on forms and ui

--- app.py
+++ app.py
@@ -42,46 +42,44 @@ class LoginForm(FlaskForm):
 
 @app.route('/')
 def index():
-    form = LoginForm()
+    # form = LoginForm()
     # check to see if the reddit api_key, client_id, and client_secret are set
     # render a form to get the api_key, client_id, and client_secret before continuing
 
     # if not all(['api_key', 'client_id', 'client_secret']) in os.environ:
     #     return render_template('form.html')
 
-
-
     # this is the main page that will render the index.html
     # get the user auth token the request header and try to verify it
     # if the token is valid, then the user will be redirected to the main page
     # if the token is invalid, then the user will be redirected to the login page
-
-
-
-
-
     # once the environment variables are set, the user needs to be redirected to login page
 
     # if not 'access_token' in os.environ:
     #     return redirect(url_for('login'))
 
     # if the user is logged in, then the user will be redirected to the main page
-    return render_template('index.html' , form=form)
+    return render_template('index.html')
 
 
-app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    form = LoginForm()
     # this is the page that will render the login form 'login.html'
     # there will be a option for a new user to create an account
 
-    render_template('login.html')
+    return render_template('login.html', form=form)
 
 
 # this will be the method that will handle the login form
-app.route('/login')
-def validate_login(request: Request ):
-    ...
-    return redirect(url_for('index'))
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    # this is the page that will render the register form 'register.html'
+    # there will be a option for a new user to create an account
+
+    return render_template('register.html', form=form)
+
 
 # need to setup db for flask
 # need to reasearch how to use flask with a database
