e6a563a841aad52f0f4a510ccd0ec2a52d2c644f
WIll Morris <88165849+Willmo103@users.noreply.github.com>
2023-03-30 10:13:53 -0500

    refactoring added api form

--- static/js/nav.js
+++ static/js/nav.js
@@ -5,26 +5,40 @@
 window.addEventListener("load", () => {
   const path = window.location.pathname;
   const search = document.getElementById("search_link");
-  const mySearches = document.getElementById("my_searches_link");
+  const mySearches = document.getElementById("mysearches_link");
   const saved = document.getElementById("saved_link");
   const logout = document.getElementById("logout_link");
   const login = document.getElementById("login_link");
   const register = document.getElementById("register_link");
 
+  const logout_html = `<a class="btn btn-outline-success my-2 my-sm-0" href="/logout"
+          ><i class="fas fa-sign-out-alt"></i> Logout</a
+        >`;
+
+  const login_html = `<a class="btn btn-outline-success my-2 my-sm-0" href="/login"
+          ><i class="fas fa-sign-in-alt"></i> Login</a
+        >`;
+
+  const register_html = `<a class="btn btn-outline-success my-2 my-sm-0" href="/register"
+          ><i class="fas fa-user-plus"></i> Register</a
+        >`;
+
   if (path === "/home" || path === "/login" || path === "/register") {
     search.style.display = "none";
     mySearches.style.display = "none";
     saved.style.display = "none";
-    logout.style.display = "none";
+    login.innerHTML = login_html;
+    register.innerHTML = register_html;
   } else if (path === "/dashboard") {
-    login.style.display = "none";
-    register.style.display = "none";
+    login.innerHTML = "";
+    register.innerHTML = "";
+    register.innerHTML = "";
   } else if (path === "/api_key") {
     search.style.display = "none";
     mySearches.style.display = "none";
     saved.style.display = "none";
-    logout.style.display = "none";
-    login.style.display = "none";
-    register.style.display = "none";
+    login.innerHTML = "";
+    register.innerHTML = "";
+    register.innerHTML = "";
   }
 });
