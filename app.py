ae706980b615b3e3abcc3a9f0eff5daa0c63ab3f
WIll Morris <88165849+Willmo103@users.noreply.github.com>
2023-03-29 23:24:41 -0500

    adding basig templates structure

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, Request, redirect, render_template, url_for
 
 app = Flask(__name__)
+app.template_folder = 'templates'
+app.static_folder = 'static'
+
+# this is the database that will store the users and their passwords
 
 
 
@@ -19,5 +23,26 @@ def index():
     if not 'access_token' in os.environ:
         return redirect(url_for('login'))
 
-
+    # if the user is logged in, then the user will be redirected to the main page
     return render_template('index.html')
+
+
+app.get('/login')
+def login():
+    # this is the page that will render the login form 'login.html'
+    # there will be a option for a new user to create an account
+
+    render_template('login.html')
+
+
+# this will be the method that will handle the login form
+app.post('/login')
+def validate_login(request: Request ):
+    ...
+    return redirect(url_for('index'))
+
+# need to setup db for flask
+# need to reasearch how to use flask with a database
+
+if __name__ == '__main__':
+    app.run(debug=True)
